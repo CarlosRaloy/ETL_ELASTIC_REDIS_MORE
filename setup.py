ec3863d6b0e3b5f8d60a73c16baa9daa7058bbc5
Carlos Garcia <cgarcia@raloy.com.mx>
2023-05-04 10:48:17 -0600

    release 0.3.2

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ readme = open("./README.md", "r")
 
 setup(
     name="redis_elastic",
-    version="0.3",
+    version="0.3.1",
     description="Crea una conexión de una base de datos postgres lo gurda en cache y en elasticsearch",
     install_requires=[
-        "ast",
         "psycopg2",
         "psycopg2-binary",
         "functools",
